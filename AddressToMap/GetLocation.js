d6942212df51ed564f32cbc54e8c977b16ea5f6d
KhoiLe1121 <khoile1121@gmail.com>
2015-04-01 12:45:04 -0700

    First Version

---
+++ AddressToMap/GetLocation.js
@@ -0,0 +1,38 @@
+﻿var getLocationApp = angular.module('getLocationApp', ['ngRoute']);
+
+getLocationApp.controller(
+    'locationManager',
+
+['$scope', '$http', function ($scope, $http) {
+    $scope.address;
+    var geocoder;
+    var map;
+
+    function initialize() {
+        geocoder = new google.maps.Geocoder();
+        var latlng = new google.maps.LatLng(0, 0);
+        var mapOptions = {
+            zoom: 2,
+            center: latlng
+        }
+        map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    }
+
+    $scope.getLocation = function() {
+        geocoder.geocode({ 'address': $scope.address }, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                map.setCenter(results[0].geometry.location);
+                map.setZoom(8);
+                var marker = new google.maps.Marker({
+                    map: map,
+                    position: results[0].geometry.location
+                });
+            } else {
+                alert("Geocode was not successful for the following reason: " + status);
+            }
+        });
+    }
+
+    initialize();
+}]);
+
